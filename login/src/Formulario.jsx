d5ef7e44582e35672c8fbca48e347455f746b1b9
DanilsGit <danielestebanmarquez12@gmail.com>
2024-05-03 23:04:05 -0500

    funcionalidad de comprobar entrada
    se ha arreglado la entrada de datos

--- login/src/Formulario.jsx
+++ login/src/Formulario.jsx
@@ -13,6 +13,36 @@ export function Formulario() {
     const [show, setShow] = useState(false)
 
     const handleClickAdd = () => {
+        if (nombre === '' || apellido === '' || edad === '') {
+            document.getElementById('1').placeholder = 'Campo requerido'
+            document.getElementById('2').placeholder = 'Campo requerido'
+            document.getElementById('3').placeholder = 'Campo requerido'
+            setTimeout(() => {
+                document.getElementById('1').placeholder = 'Nombre'
+                document.getElementById('2').placeholder = 'Apellido'
+                document.getElementById('3').placeholder = 'Edad'
+            }, 2000)
+            return
+        }
+        if (nombre.length < 3 || apellido.length < 3) {
+            document.getElementById('1').value = ''
+            document.getElementById('2').value = ''
+            document.getElementById('1').placeholder = 'Nombre muy corto'
+            document.getElementById('2').placeholder = 'Apellido muy corto'
+            setTimeout(() => {
+                document.getElementById('1').placeholder = 'Nombre'
+                document.getElementById('2').placeholder = 'Apellido'
+            }, 2000)
+            return
+        }
+        if (edad < 18) {
+            document.getElementById('3').value = ''
+            document.getElementById('3').placeholder = 'Debes ser mayor de edad'
+            setTimeout(() => {
+                document.getElementById('3').placeholder = 'Edad'
+            }, 2000)
+            return
+        }
         setUsers([...users, { nombre, apellido, edad }])
         console.log(users)
     }
@@ -25,9 +55,9 @@ export function Formulario() {
         <div className='contenedor'>
             <div className='formulario'>
                 <h2>Ingresa tus datos</h2>
-                <input placeholder='nombre' type="text" onChange={(e) => { setNombre(e.target.value) }} />
-                <input placeholder='apellido' type="text" onChange={(e) => { setApellido(e.target.value) }} />
-                <input placeholder='edad' type="number" onChange={(e) => { setEdad(e.target.value) }} />
+                <input id='1' placeholder='nombre' type="text" onChange={(e) => { setNombre(e.target.value) }} />
+                <input id='2' placeholder='apellido' type="text" onChange={(e) => { setApellido(e.target.value) }} />
+                <input id='3' placeholder='edad' type="number" onChange={(e) => { setEdad(e.target.value) }} />
 
                 <button onClick={handleClickAdd} >Agregar Usuario</button>
                 <button onClick={handleClickShow} >Mostrar Usuarios</button>
